dd521b023e5b48d321761196f34d737917698b3e
Thomas Styron <thomasstyron@gmail.com>
2023-05-04 20:36:38 -0500

    14.7

---
+++ requests-in-javascript/requests.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+const url = "http://localhost:5001/constellations/";
+axios
+//   .get(url)
+//   .then((response) => {
+//     console.log(response.status);
+//     console.log(response.statusText);
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   })
+// axios.get(url)
+//     .then((response)=>{
+//         const arr = response.data.filter((constellation)=>{
+//           return constellation.starsWithPlanets < 10  
+//         }) 
+//         console.log(arr) 
+//     })
+//     .catch((error)=>{
+//         console.log(error.message)
+//     })
+
